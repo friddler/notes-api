cf806a45aedf749e21f169291e55b9ee77ee291b
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-11-29 15:28:11 +0100

    delete function created. doesnt work right now

--- functions/deleteNotes/index.js
+++ functions/deleteNotes/index.js
@@ -1,7 +1,60 @@
 const { sendResponse } = require("../../responses/index");
 const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient();
-const { nanoid } = require("nanoid");
-const middy = require('@middy/core');
+const middy = require("@middy/core");
 const { validateToken } = require("../middleware/auth");
-const jwt = require('jsonwebtoken');
+
+const deleteNote = async (event, context) => {
+  if (event?.error && event.error === "401") {
+    return sendResponse(401, { success: false, message: "Invalid token" });
+  }
+
+  const requestBody = JSON.parse(event.body);
+  const noteId = requestBody.id;
+  const userName = event.username;
+
+  if (!noteId) {
+    return sendResponse(400, {
+      success: false,
+      message: "Note ID is required",
+    });
+  }
+
+  try {
+    const noteData = await db
+      .get({
+        TableName: "notes-db",
+        Key: { id: noteId },
+      })
+      .promise();
+
+    if (!noteData.Item || noteData.Item.username !== userName) {
+      return sendResponse(403, {
+        success: false,
+        message: "Access failed or note not found",
+      });
+    }
+
+    await db
+      .delete({
+        TableName: "notes-db",
+        Key: { id: noteId },
+      })
+      .promise();
+
+    return sendResponse(200, {
+      success: true,
+      message: "Note successfully deleted",
+    });
+  } catch (error) {
+    console.error("Error deleting note:", error);
+    return sendResponse(500, {
+      success: false,
+      message: "Failed to delete note",
+    });
+  }
+};
+
+const handler = middy(deleteNote).use(validateToken);
+
+module.exports = { handler };
